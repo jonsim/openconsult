a56e79853019781b5e1132aad2b4a6bb84d5d3aa
Jonathan Simmonds <jonathansimmonds@gmail.com>
2023-01-10 22:13:06 +0000

    Add initial unit test
    Not 100% about the layout. Don't love it. Trying to do it as Bazely as
    possible, but Bazel is fighting me a bit. And I don't want to go down
    the 'dump all your source, tests and headers in one dir' route that the
    Google-maintained projects seem to follow to fix it.

---
+++ WORKSPACE
@@ -0,0 +1,7 @@
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+
+http_archive(
+  name = "gtest",
+  urls = ["https://github.com/google/googletest/archive/release-1.12.1.zip"],
+  strip_prefix = "googletest-release-1.12.1",
+)
---
+++ lib/test/log_replay.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include "lib/src/log_replay.h"
+
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+}
+
+TEST(LogReplayTest, Basic) {
+    LogReplay foo("boogle.txt");
+    EXPECT_EQ(NULL, foo);
+}
